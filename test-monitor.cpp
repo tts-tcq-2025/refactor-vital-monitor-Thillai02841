7028ae30070570512378002b042cec3f647c1f19
Thillai02841 <thillai240201@gmail.com>
2025-08-22 10:06:44 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,29 +1,21 @@
-// #include <gtest/gtest.h>
-// #include "./monitor.h"
-
-// TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-//   ASSERT_FALSE(vitalsOk(99, 102, 70));
-//   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
-// }
-
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));  // Pulse out of range
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));  // All in range
+  ASSERT_FALSE(vitalsOk(99, 102, 70));   // Pulse out of range
+  ASSERT_TRUE(vitalsOk(98.1, 70, 98));   // All in range
 }
 
 TEST(Monitor, TemperatureLowShouldFail) {
-  ASSERT_FALSE(vitalsOk(94, 70, 98));   // Low temperature
+  ASSERT_FALSE(vitalsOk(94, 70, 98));    // Low temperature
 }
 
 TEST(Monitor, TemperatureHighShouldFail) {
-  ASSERT_FALSE(vitalsOk(103, 70, 98));  // High temperature
+  ASSERT_FALSE(vitalsOk(103, 70, 98));   // High temperature
 }
 
 TEST(Monitor, PulseLowShouldFail) {
-  ASSERT_FALSE(vitalsOk(98.6, 55, 98)); // Low pulse
+  ASSERT_FALSE(vitalsOk(98.6, 55, 98));  // Low pulse
 }
 
 TEST(Monitor, PulseHighShouldFail) {
@@ -31,5 +23,5 @@ TEST(Monitor, PulseHighShouldFail) {
 }
 
 TEST(Monitor, Spo2LowShouldFail) {
-  ASSERT_FALSE(vitalsOk(98.6, 72, 85)); // Low SpO₂
+  ASSERT_FALSE(vitalsOk(98.6, 72, 85));  // Low SpO₂
 }
